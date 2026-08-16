16b4b6a2cc8a5146c1e908a08060aaf2cb37c163
amisin <your-email@example.com>
2026-08-16 14:52:40 +0530

    sorting data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app=FastAPI()
 
@@ -19,3 +19,23 @@ def about():
 def view():
     data=load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str=Path(...,description='ID of patient in DB',example='P001')):
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail='patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by:str=Query(...,description='sort on the basis of height,wieght,bmi'),order:str=Query('asc',description='sort in asc or desc')):
+    valid_fields=['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail='invalid field')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='invalid order')
+
+    data=load_data()
+    sort_order=True if order=='desc' else False
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
