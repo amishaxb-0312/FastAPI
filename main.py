f932b0764362a21500ad208ec1dc0d4e8ada430c
amisin <your-email@example.com>
2026-08-16 14:06:13 +0530

    changes made

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-
+import json
 app=FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {'message':'helloooo'}
+    return {'message':'Patient management system api'}
 
 @app.get('/about')
 def about():
-    return {'message':'Great to see you'}
+    return {'message':'fully functional api'}
+
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
